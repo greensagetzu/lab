74c56fad6d826ceae85c8d0ffa5ad8080d8c1dd4
Sage Liu <sagectliu@gmail.com>
2022-11-16 01:34:23 -0600

    Add docstrings and type hinting

--- account.py
+++ account.py
@@ -1,20 +1,47 @@
 
 class Account:
-	def __init__(self, name):
+	
+	def __init__(self, name) -> None:
+		"""[Inits account ]
+
+		Args:
+			name ([string]): [person's name]
+		"""
 		self.__name = name
 		self.__balance = 0
 
-	def get_balance(self):
+	def get_balance(self) -> int:
+		"""[Function to get account balance]
+
+		Returns:
+			[int]: [account balance]
+		"""
 		return self.__balance
 
-	def deposit(self, amount):
+	def deposit(self, amount) -> boolean:
+		"""[Function to increment the account balance]
+				
+		Args:
+			amount ([int]): [amount should be added to account]
+		
+		Returns:
+			bool: [if deposit transaction is successful or not]
+		"""
 		if amount < 0 or amount == 0:
 			return False
 		else:
 			self.__balance += amount
 			return True
 
-	def withdraw(self, amount):
+	def withdraw(self, amount) -> boolean:
+		"""[Function to decrement the account balance]
+		
+		Args:
+			amount ([int]): [amount should be subtracted to account]
+		
+		Returns:
+			bool: [if withdraw transaction is successful or not]
+		"""
 		if amount < 0 or amount == 0:
 			return False
 		elif amount > self.__balance:
@@ -23,14 +50,24 @@ class Account:
 			self.__balance -= amount
 			return True
 
-	def get_name(self):
+	def get_name(self) -> string:
+		"""[Function to get account name]
+		
+		Returns:
+			[string]: [account name]
+		"""
 		return self.__name
 
-	def __str__(self):
+	def __str__(self) -> string:
+		"""[Function to print]
+		
+		Returns:
+			[string]: [print]
+		"""
 		return f'account_name = {self.__name} and account_balance = {self.__balance}'
 
 
-def main():
+def main() -> none:
 	account_one = Account('001-John')
 	
 	account_one.deposit(100)
