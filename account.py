a848a9be6dfe361fb516ddef8782481b5db7364c
Sage Liu <sagectliu@gmail.com>
2022-11-17 14:02:53 -0600

    add new file- 1.2

--- account.py
+++ account.py
@@ -1,53 +1,55 @@
-
 class Account:
 	def __init__(self, name):
-		self.__name = name
-		self.__balance = 0
+		self.__account_name = name
+		self.__account_balance = 0
 
 	def get_balance(self):
-		return self.__balance
+		return self.__account_balance
 
 	def deposit(self, amount):
 		if amount < 0 or amount == 0:
 			return False
 		else:
-			self.__balance += amount
+			self.__account_balance += amount
 			return True
 
 	def withdraw(self, amount):
 		if amount < 0 or amount == 0:
 			return False
-		elif amount > self.__balance:
+		elif amount > self.__account_balance:
 			return False
 		else:
-			self.__balance -= amount
+			self.__account_balance -= amount
 			return True
 
 	def get_name(self):
-		return self.__name
+		return self.__account_name
 
-	def __str__(self):
-		return f'account_name = {self.__name} and account_balance = {self.__balance}'
+	#def __str__(self):
+	 	#return f'account_name = {self.__account_name} and account_balance = {self.__account_balance}'
 
 
+#checking the code can be run
 def main():
 	account_one = Account('001-John')
 	
-	account_one.deposit(100)
-	account_one.deposit(200)
+	account_one.deposit(1000)
+	account_one.deposit(-250)
+	account_one.deposit(0)
 	account_one.withdraw(400)
-	account_one.withdraw(50)
+	account_one.withdraw(-50)
+	account_one.withdraw(0)
 
 	print(account_one)
 
 	name = input('Enter the name: ')
 	account_two = Account(name)
 
-	deposit_num = int(input('Enter amount to be desposited: '))
+	deposit_num = float(input('Enter amount to be desposited: '))
 	account_two.deposit(deposit_num)
 	print(account_two)
 
-	withdraw_num = int(input('Enter amount to be withdrawn: '))
+	withdraw_num = float(input('Enter amount to be withdrawn: '))
 	account_two.withdraw(withdraw_num)
 	print(account_two)
 
