b3d515bbf83e0c5591313dcebfe19ed41b7548a6
Sage Liu <sagectliu@gmail.com>
2022-11-17 01:11:33 -0600

    add second file- 1.1

--- test_account.py
+++ test_account.py
@@ -1,3 +1,4 @@
+import pytest
 from account import *
 
 class Test:
@@ -15,20 +16,49 @@ class Test:
 		assert self.p2.get_name() == 'John'
 		assert self.p2.get_balance() == 0
 
-		assert self.p1.__str__() == 'account_name = Jane and account_balance = 0'
-		assert self.p2.__str__() == 'account_name = John and account_balance = 0'
-
 	def test_deposit(self):
 		assert self.p1.deposit(500) == True
 		assert self.p1.get_balance()  == 500
 		assert self.p1.deposit(-50) == False
+		assert self.p1.get_balance()  == 500
 		assert self.p1.deposit(0) == False
-		assert self.p1.deposit(388) == True
-		assert self.p1.get_balance()  == 888
+		assert self.p1.get_balance()  == 500
+		assert self.p1.deposit(388.8) == True
+		assert self.p1.get_balance()  == pytest.approx(888.8, abs=0.001)
+
+		assert self.p2.deposit(1000) == True
+		assert self.p2.get_balance()  == 1000
+		assert self.p2.deposit(-500) == False
+		assert self.p2.get_balance()  == 1000
+		assert self.p2.deposit(0) == False
+		assert self.p2.get_balance()  == 1000
+		assert self.p2.deposit(555.5) == True
+		assert self.p2.get_balance()  == pytest.approx(1555.5, abs=0.001)
 
 	def test_withdraw(self):
+
 		assert self.p1.withdraw(500) == False
+		assert self.p1.get_balance()  == 0
 		assert self.p1.withdraw(-50) == False
+		assert self.p1.get_balance()  == 0
 		assert self.p1.withdraw(0) == False
 		assert self.p1.get_balance()  == 0
+
+		self.p1.deposit(500) 
+		assert self.p1.withdraw(20.5) == True
+		assert self.p1.get_balance() == pytest.approx(479.5, abs=0.001)
+
+		assert self.p2.withdraw(250) == False
+		assert self.p2.get_balance()  == 0
+		assert self.p2.withdraw(-5) == False
+		assert self.p2.get_balance()  == 0
+		assert self.p2.withdraw(0) == False
+		assert self.p2.get_balance()  == 0
+
+		self.p2.deposit(1700) 
+		assert self.p2.withdraw(500.25) == True
+		assert self.p2.get_balance() == pytest.approx(1199.75, abs=0.001)
+
+
+		
 
