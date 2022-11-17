9ce19de9b6869bc5ffc1abbe8cb7638624d386a6
Sage Liu <sagectliu@gmail.com>
2022-11-17 13:56:19 -0600

    add new file 1.1

--- account.py
+++ account.py
@@ -1,45 +1,20 @@
 
 class Account:
-	
-	def __init__(self, name) -> None:
-		"""
-		Inits account that set the default values
-
-		:param account_name: person's name
-		"""
+	def __init__(self, name):
 		self.__account_name = name
 		self.__account_balance = 0
 
-	def get_balance(self) -> float:
-		"""
-		Function to get account balance
-
-		:return: account balance
-		"""
+	def get_balance(self):
 		return self.__account_balance
 
-	def deposit(self, amount:float) -> bool:
-		"""
-		Function to increment the account balance
-				
-		:param amount: amount should be added to account
-		
-		:return: return True if deposit transaction is successful and False if deposit transaction is unsuccessful
-		"""
+	def deposit(self, amount):
 		if amount < 0 or amount == 0:
 			return False
 		else:
 			self.__account_balance += amount
 			return True
 
-	def withdraw(self, amount:float) -> bool:
-		"""
-		Function to decrement the account balance
-		
-		:param amount: amount should be subtracted to account
-		
-		:return: return True if withdraw transaction is successful and False if withdraw transaction is unsuccessfult
-		"""
+	def withdraw(self, amount):
 		if amount < 0 or amount == 0:
 			return False
 		elif amount > self.__account_balance:
@@ -48,14 +23,37 @@ class Account:
 			self.__account_balance -= amount
 			return True
 
-	def get_name(self) -> str:
-		"""
-		Function to get account name
-		
-		:return: account name
-		"""
+	def get_name(self):
 		return self.__account_name
 
+	#def __str__(self):
+	 	#return f'account_name = {self.__account_name} and account_balance = {self.__account_balance}'
 
 
+#checking the code can be run
+def main():
+	account_one = Account('001-John')
+	
+	account_one.deposit(1000)
+	account_one.deposit(-250)
+	account_one.deposit(0)
+	account_one.withdraw(400)
+	account_one.withdraw(-50)
+	account_one.withdraw(0)
 
+	print(account_one)
+
+	name = input('Enter the name: ')
+	account_two = Account(name)
+
+	deposit_num = float(input('Enter amount to be desposited: '))
+	account_two.deposit(deposit_num)
+	print(account_two)
+
+	withdraw_num = float(input('Enter amount to be withdrawn: '))
+	account_two.withdraw(withdraw_num)
+	print(account_two)
+
+
+if __name__ == '__main__':
+	main()
