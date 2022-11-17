00989b50c5d4280faf9d7cd553c4c3f6497f7e99
Sage Liu <sagectliu@gmail.com>
2022-11-17 01:09:51 -0600

    add first file- 1.1

--- account.py
+++ account.py
@@ -2,30 +2,29 @@
 class Account:
 	
 	def __init__(self, name) -> None:
-		"""[Inits account ]
+		"""
+		Inits account that set the default values
 
-		Args:
-			name ([string]): [person's name]
+		:param name: person's name
 		"""
 		self.__name = name
 		self.__balance = 0
 
-	def get_balance(self) -> int:
-		"""[Function to get account balance]
+	def get_balance(self) -> float:
+		"""
+		Function to get account balance
 
-		Returns:
-			[int]: [account balance]
+		:return: account balance
 		"""
 		return self.__balance
 
 	def deposit(self, amount) -> bool:
-		"""[Function to increment the account balance]
+		"""
+		Function to increment the account balance
 				
-		Args:
-			amount ([int]): [amount should be added to account]
+		:param amount: amount should be added to account
 		
-		Returns:
-			bool: [if deposit transaction is successful or not]
+		:return: return True if deposit transaction is successful and False if deposit transaction is unsuccessful
 		"""
 		if amount < 0 or amount == 0:
 			return False
@@ -34,13 +33,12 @@ class Account:
 			return True
 
 	def withdraw(self, amount) -> bool:
-		"""[Function to decrement the account balance]
+		"""
+		Function to decrement the account balance
 		
-		Args:
-			amount ([int]): [amount should be subtracted to account]
+		:param amount: amount should be subtracted to account
 		
-		Returns:
-			bool: [if withdraw transaction is successful or not]
+		:return: return True if withdraw transaction is successful and False if withdraw transaction is unsuccessfult
 		"""
 		if amount < 0 or amount == 0:
 			return False
@@ -51,43 +49,13 @@ class Account:
 			return True
 
 	def get_name(self) -> str:
-		"""[Function to get account name]
+		"""
+		Function to get account name
 		
-		Returns:
-			[string]: [account name]
+		:return: account name
 		"""
 		return self.__name
 
-	def __str__(self) -> str:
-		"""[Function to print]
-		
-		Returns:
-			[string]: [print]
-		"""
-		return f'account_name = {self.__name} and account_balance = {self.__balance}'
 
 
-def main() -> None:
-	account_one = Account('001-John')
-	
-	account_one.deposit(100)
-	account_one.deposit(200)
-	account_one.withdraw(400)
-	account_one.withdraw(50)
 
-	print(account_one)
-
-	name = input('Enter the name: ')
-	account_two = Account(name)
-
-	deposit_num = int(input('Enter amount to be desposited: '))
-	account_two.deposit(deposit_num)
-	print(account_two)
-
-	withdraw_num = int(input('Enter amount to be withdrawn: '))
-	account_two.withdraw(withdraw_num)
-	print(account_two)
-
-
-if __name__ == '__main__':
-	main()
